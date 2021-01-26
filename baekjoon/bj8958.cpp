08da43d419986b884860483afbc118c7424db5ae
Kimsuhhee <kimsu3819@gmail.com>
2021-01-26 22:27:32 +0900

    baekjoon:8958_OX퀴즈

---
+++ baekjoon/bj8958.cpp
@@ -0,0 +1,27 @@
+﻿/*
+문제:백준8958번
+출처:https://www.acmicpc.net/problem/8958
+*/
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+int main() {
+	int n;
+	char s[80];
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int cnt = 0;
+		int sum = 0;
+		cin >> s;
+		for (int j = 0; j < strlen(s); j++) {
+			if (s[j] == 'O') {
+				cnt++;
+				sum += cnt;
+			}
+			else cnt = 0;
+		}
+		cout << sum << '\n';
+	}
+	
+}
