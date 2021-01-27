42af637b074681b39f33a624fefa13aed5280e45
Kimsuhhee <kimsu3819@gmail.com>
2021-01-27 20:53:31 +0900

    baekjoon:1152_단어의개수

---
+++ baekjoon/bj1152.cpp
@@ -0,0 +1,21 @@
+﻿/*
+문제:백준1152번
+출처:https://www.acmicpc.net/problem/1152
+*/
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+int main() {
+	char s[1000000];
+	int cnt = 0;
+	cin.getline(s, 1000000);
+	int len = strlen(s);
+	for (int i = 0; i < len; i++) {
+		char c = s[i];
+		if (c == ' ')cnt++;
+	}
+	if (s[0] == ' ')cnt--; //맨앞공백
+	if (s[len-1] == ' ')cnt--; //맨뒤공백
+	cout << cnt+1 << '\n';
+}
