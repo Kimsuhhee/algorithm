2ab90c17b3a2d07b065218af7ee334e75f3eebe4
Kimsuhhee <kimsu3819@gmail.com>
2021-01-27 23:39:28 +0900

    baekjoon:2292_벌집

---
+++ baekjoon/bj2292.cpp
@@ -0,0 +1,19 @@
+﻿/*
+문제:백준2292번
+출처:https://www.acmicpc.net/problem/2292
+*/
+#include<iostream>
+using namespace std;
+
+int main() {
+	int N;
+	int sum = 1;
+	int res = 1;
+	cin >> N;
+	while (1) {
+		if (N <= sum)break;
+		sum += 6 * res;
+		res++;
+	}
+	cout << res;
+}
