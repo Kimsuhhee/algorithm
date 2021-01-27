da6916276aaa243e71cf45d57496fd2ca42a3850
Kimsuhhee <kimsu3819@gmail.com>
2021-01-27 22:49:29 +0900

    baekjoon:10872_팩토리얼

---
+++ baekjoon/bj10872.cpp
@@ -0,0 +1,20 @@
+﻿/*
+문제:백준10872번
+출처:https://www.acmicpc.net/problem/10872
+*/
+#include<iostream>
+using namespace std;
+
+int factorial(int N) {
+	if (N > 2)
+		N *= factorial(N - 1);
+	return N;
+}
+int main() {
+	int N;
+	cin >> N;
+	if (N == 0)
+		cout << 1 << '\n';
+	else 
+		cout << factorial(N) << '\n';
+}
