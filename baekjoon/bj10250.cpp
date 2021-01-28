4ea4a3702e51e677912f19b66c610855d58949a6
Kimsuhhee <kimsu3819@gmail.com>
2021-01-28 16:33:25 +0900

    baekjoon:10250_ACM호텔

---
+++ baekjoon/bj10250.cpp
@@ -0,0 +1,25 @@
+﻿/*
+문제:백준10250번
+출처:https://www.acmicpc.net/problem/10250
+*/
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+	int h, w, n;
+	int f,d; 
+	int T;
+	cin >> T;
+	while (T--) {
+		cin >> h >> w >> n;
+		d = n / h; 
+		f = n % h; 
+		if (f == 0) {
+			cout << h * 100 + (d) << '\n';
+		}
+		else {
+			cout << f * 100 + (d + 1) << '\n';
+		}
+	}
+}
