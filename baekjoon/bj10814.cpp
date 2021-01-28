95fbd8a81223acec1a92fd62fd53b8207d310463
Kimsuhhee <kimsu3819@gmail.com>
2021-01-28 18:57:33 +0900

    baekjoon:10814_나이순정렬

---
+++ baekjoon/bj10814.cpp
@@ -0,0 +1,28 @@
+﻿/*
+문제:백준10814번
+출처:https://www.acmicpc.net/problem/10814
+*/
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+bool compare(pair<int, string>a, pair<int, string>b) {
+	return a.first < b.first;
+}
+
+int main() {
+	int N;
+	cin >> N;
+	vector<pair<int, string>>v;
+	int age; string name;
+	for (int i = 0; i < N; i++) {
+		cin >> age >> name;
+		v.push_back(make_pair(age, name));
+	}
+	stable_sort(v.begin(), v.end(), compare);
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i].first << " " << v[i].second << '\n';
+	}
+}
