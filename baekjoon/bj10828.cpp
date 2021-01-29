3f35a39017f4ee1506693e52cee5f264feed162d
Kimsuhhee <kimsu3819@gmail.com>
2021-01-29 22:55:57 +0900

    baekjoon:10828_스택

---
+++ baekjoon/bj10828.cpp
@@ -0,0 +1,40 @@
+﻿/*
+문제:백준10828번
+출처:https://www.acmicpc.net/problem/10828
+*/
+#include<iostream>
+#include<stack>
+#include<string>
+using namespace std;
+
+int main() {
+	int N;
+	string c; int num;
+	cin >> N;
+	stack<int> s;
+	while (N--) {
+		cin >> c;
+		if (c == "push") {
+			cin >> num;
+			s.push(num);
+		}
+		else if (c == "pop") {
+			if (!s.empty()) {
+				cout << s.top() << '\n';
+				s.pop();
+			}
+			else cout << -1 << '\n';
+		}
+		else if (c == "size") {
+			cout << s.size() << '\n';
+		}
+		else if (c == "empty") {
+			if (s.empty())cout << 1 << '\n';
+			else cout << 0 << '\n';
+		}
+		else {
+			if (!s.empty()) cout << s.top() << '\n';
+			else cout << -1 << '\n';
+		}
+	}
+}
