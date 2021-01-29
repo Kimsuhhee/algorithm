d8d5342462d07202d596d08c4c147b6b21afc493
Kimsuhhee <kimsu3819@gmail.com>
2021-01-29 18:50:15 +0900

    baekjoon:11650_좌표정렬하기

---
+++ baekjoon/bj11650.cpp
@@ -0,0 +1,29 @@
+﻿/*
+문제:백준11650번
+출처:https://www.acmicpc.net/problem/11650
+*/
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+bool compare(pair<int, int>a, pair<int, int>b) {
+	if (a.first == b.first)return a.second < b.second;
+	return a.first < b.first;
+}
+
+int main() {
+	int N;
+	cin >> N;
+	vector<pair<int, int>>v;
+	int x, y;
+	for (int i = 0; i < N; i++) {
+		cin >> x >> y;
+		v.push_back(make_pair(x, y));
+	}
+	stable_sort(v.begin(), v.end(), compare);
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i].first << " " << v[i].second << '\n';
+	}
+}
