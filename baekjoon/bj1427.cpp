9d71793c4673b67105cda019e299caf7e245514b
Kimsuhhee <kimsu3819@gmail.com>
2021-01-29 20:10:08 +0900

    baekjoon:1427_소트인사이드

---
+++ baekjoon/bj1427.cpp
@@ -0,0 +1,16 @@
+﻿/*
+문제:백준1427번
+출처:https://www.acmicpc.net/problem/1427
+*/
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+int main() {
+	string s;
+	cin >> s;
+	sort(s.begin(), s.end());
+	reverse(s.begin(), s.end());
+	cout << s;
+}
