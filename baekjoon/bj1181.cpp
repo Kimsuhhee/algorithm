241899d35535273ed2268f138fc5defea55ab966
Kimsuhhee <kimsu3819@gmail.com>
2021-01-29 19:43:21 +0900

    baekjoon:1181_단어정렬

---
+++ baekjoon/bj1181.cpp
@@ -0,0 +1,33 @@
+﻿/*
+문제:백준1181번
+출처:https://www.acmicpc.net/problem/1181
+*/
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+bool compare(string a, string b) {
+	if (a.size() == b.size()) { //문자열의 길이가 같을경우
+		return a < b; //사전순서
+	}
+	else //문자열의 길이가 다른경우
+		return a.size() < b.size();	//문자열의 길이가 짧은순서
+}
+
+int main() {
+	int N;
+	cin >> N;
+	vector<string>v;
+	string s;
+	for (int i = 0; i < N; i++) {
+		cin >> s;
+		v.push_back(s);
+	}
+	stable_sort(v.begin(), v.end(), compare);
+	v.erase(unique(v.begin(), v.end()), v.end());
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i] << '\n';
+	}
+}
