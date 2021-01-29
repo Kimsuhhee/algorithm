e9d15cd9813633545524cf26158309eaeaa18952
Kimsuhhee <kimsu3819@gmail.com>
2021-01-29 22:56:24 +0900

    baekjoon:10773_제로

---
+++ baekjoon/bj10773.cpp
@@ -0,0 +1,27 @@
+﻿/*
+문제:백준10773번
+출처:https://www.acmicpc.net/problem/10773
+*/
+#include<iostream>
+#include<stack>
+#include<string>
+using namespace std;
+
+int main() {
+	stack<int>s;
+	int N, a;
+	int sum = 0;
+	cin >> N;
+	while (N--) {
+		cin >> a;
+		if (a == 0) {
+			sum -= s.top();
+			s.pop();
+		}
+		else {
+			s.push(a);
+			sum += a;
+		}
+	}
+	cout << sum << '\n';
+}
