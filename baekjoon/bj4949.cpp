f98133abc47766c90e8699c5247143ea0a9095cc
Kimsuhhee <kimsu3819@gmail.com>
2021-01-30 19:54:33 +0900

    baekjoon:4949_균형잡힌세상

---
+++ baekjoon/bj4949.cpp
@@ -0,0 +1,43 @@
+﻿/*
+문제:백준4949번
+출처:https://www.acmicpc.net/problem/4949
+*/
+#include<iostream>
+#include<stack>
+#include<cstring>
+using namespace std;
+
+bool isBalance(char c[]) {
+	stack<char> s;
+	int len = strlen(c);
+	for (int i = 0; i < len ; i++) {
+		if (c[i] == '(' || c[i] == '[') s.push(c[i]);
+		else if (c[i] == ')') {
+			if (!s.empty() && s.top() == '(') {
+				s.pop();
+			}
+			else return false;
+		}
+		else if (c[i] == ']') {
+			if (!s.empty() && s.top() == '[') {
+				s.pop();
+			}
+			else return false;
+		}
+	}
+	if (!s.empty()) return false;
+	return true;
+}
+
+int main() {
+	string s;
+	char c[101];
+
+	while (cin.getline(c, 101)) {
+		if (c[0] == '.')break;
+		if (isBalance(c))
+			cout << "yes" << '\n';
+		else
+			cout << "no" << '\n';
+	}
+}
