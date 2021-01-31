74982360775266b4bfd1cce4ec90688ad32908e7
Kimsuhhee <kimsu3819@gmail.com>
2021-01-31 18:57:55 +0900

    baekjoon:11866_요세푸스문제0

---
+++ baekjoon/bj11866.cpp
@@ -0,0 +1,35 @@
+﻿/*
+문제:백준11866번
+출처:https://www.acmicpc.net/problem/11866
+*/
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int main() {
+	queue<int>q;
+	int N, K;
+	cin >> N >> K;
+	for (int i = 1; i <= N; i++) {
+		q.push(i);
+	}
+	//1234567
+	cout << "<";
+	while (!q.empty()) {
+		for (int i = 1; i <= K; i++) {
+			int temp = q.front();
+
+			if (i == K) {
+				q.pop();
+				cout << temp;
+				if (!q.empty()) cout << ", ";
+			}
+			else {
+				q.pop();
+				q.push(temp);
+			}
+		}
+	}
+
+	cout << ">";
+}
