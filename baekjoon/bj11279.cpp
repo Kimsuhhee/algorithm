c56360fd679f23b4e7e1632b889c405f6ecda38c
Kimsuhhee <kimsu3819@gmail.com>
2021-02-02 23:33:34 +0900

    baekjoon:11279_최대힙

---
+++ baekjoon/bj11279.cpp
@@ -0,0 +1,29 @@
+﻿/*
+문제:백준11279번
+출처:https://www.acmicpc.net/problem/11279
+*/
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	priority_queue<int>pq;
+	int N, x;
+	cin >> N;
+	while (N--) {
+		cin >> x;
+		if (x == 0) {
+			if (!pq.empty()) {
+				int temp = pq.top();
+				cout << temp << '\n';
+				pq.pop();
+			}
+			else cout << 0 << '\n';
+		}
+		else {
+			pq.push(x);
+		}
+	}
+}
