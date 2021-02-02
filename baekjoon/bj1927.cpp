c6187fa640fa0e0e7ec218ad0fa91abe35bc537d
Kimsuhhee <kimsu3819@gmail.com>
2021-02-02 23:34:00 +0900

    baekjoon:1927_최소힙

---
+++ baekjoon/bj1927.cpp
@@ -0,0 +1,30 @@
+﻿/*
+문제:백준1927번
+출처:https://www.acmicpc.net/problem/1927
+*/
+#include<iostream>
+#include<queue>
+#include<vector>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	priority_queue<int,vector<int>,greater<int>>pq;
+	int N, x;
+	cin >> N;
+	while (N--) {
+		cin >> x; 
+		if (x == 0) {
+			if (!pq.empty()) {
+				int temp = pq.top();
+				cout << temp << '\n';
+				pq.pop();
+			}
+			else cout << 0 << '\n';
+		}
+		else {
+			pq.push(x);
+		}
+	}
+}
