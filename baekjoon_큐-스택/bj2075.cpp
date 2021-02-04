37b3f3736bb56249cc35ef4e05817330259099fd
Kimsuhhee <kimsu3819@gmail.com>
2021-02-04 23:29:58 +0900

    baekjoon:2075_N번째큰수

---
+++ baekjoon_큐-스택/bj2075.cpp
@@ -0,0 +1,27 @@
+﻿/*
+문제:백준2075번
+출처:https://www.acmicpc.net/problem/2075
+*/
+#include <string>
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N, x;
+	priority_queue<long, vector<long>, greater<long>> pq;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cin >> x;
+			pq.push(x);
+			if (pq.size() > N)pq.pop();
+		}
+	}
+	cout << pq.top();
+}
