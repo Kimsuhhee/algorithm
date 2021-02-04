941bc3ddd68891149d4e16c67c4a59adfc4de46a
Kimsuhhee <kimsu3819@gmail.com>
2021-02-04 23:26:16 +0900

    baekjoon:1978_소수찾기

---
+++ baekjoon/bj1978.cpp
@@ -0,0 +1,26 @@
+﻿/*
+문제:백준1978번
+출처:https://www.acmicpc.net/problem/1978
+*/
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+	int N,x,i;
+	int cnt = 0;
+	cin >> N;
+	while (N--) {
+		cin >> x;
+		if (x == 1)continue;
+		else if (x == 2)cnt++;
+		else{
+			for (i = 2; i < x; i++) {
+				if (x % i == 0)break; //1과 자신외의 수로 나눠지면 소수 아님
+				
+			}
+			if (x == i)cnt++; 
+		}
+	}
+	cout << cnt << '\n';
+}
