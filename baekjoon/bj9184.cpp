90c83df9452f62016c702db992f6f32f9df75692
Kimsuhhee <kimsu3819@gmail.com>
2021-02-09 23:35:20 +0900

    baekjoon:9184_신나는함수실행

---
+++ baekjoon/bj9184.cpp
@@ -0,0 +1,28 @@
+﻿/*
+문제:백준9184번
+출처:https://www.acmicpc.net/problem/9184
+*/
+#include<iostream>
+#include<cstring>
+using namespace std;
+int d[50][50][50];
+int w(int a, int b, int c) {
+	if (a <= 0 || b <= 0 || c <= 0)return 1;
+	else if (a > 20 || b > 20 || c > 20)return w(20, 20, 20);
+	else if (a < b && b < c) {
+		if (d[a][b][c] != 0)return d[a][b][c];
+		else return d[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+	}
+	else {
+		if (d[a][b][c] != 0)return d[a][b][c];
+		else return d[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+	}
+}
+
+int main() {
+	int a, b, c;
+	while (cin >> a >> b >> c) {
+		if (a == -1 && b == -1 && c == -1)break;
+		cout << "w(" << a << ", " << b << ", " << c << ") = " << w(a, b, c) << '\n';
+	}
+}
