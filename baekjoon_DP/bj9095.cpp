56ca573d63137b0a435cb6d110adb106675fff8a
Kimsuhhee <kimsu3819@gmail.com>
2021-02-12 23:13:46 +0900

    baekjoon:9095_1,2,3더하기

---
+++ baekjoon_DP/bj9095.cpp
@@ -0,0 +1,28 @@
+﻿/*
+문제:백준9095번
+출처:https://www.acmicpc.net/problem/9095
+*/
+#include<iostream>
+#include<cstring>
+using namespace std;
+int d[11];
+int T, n;
+
+void dp() {
+	d[1] = 1;
+	d[2] = 2;
+	d[3] = 4;
+	for (int i = 4; i <= n; i++) {
+		d[i] = d[i - 1] + d[i - 2] + d[i - 3];
+	}
+	cout << d[n] << '\n';
+}
+
+int main() {
+	cin >> T;
+	while (T--) {
+		cin >> n;
+		dp();
+		memset(d, 0, sizeof(d));
+	}
+}
