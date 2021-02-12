cbe4d44d3b714996cd665bbd8bb20428229da233
Kimsuhhee <kimsu3819@gmail.com>
2021-02-12 23:13:15 +0900

    baekjoon:2156_포도주시식

---
+++ baekjoon/bj2156.cpp
@@ -0,0 +1,26 @@
+﻿/*
+문제:백준2156번
+출처:https://www.acmicpc.net/problem/2156
+*/
+#include<iostream>
+#include<algorithm>
+#include<cstring>
+using namespace std;
+int d[10001];
+int graph[10001];
+int n;
+
+void dp() {
+	d[1] = graph[1];
+	d[2] = d[1] + graph[2];
+	for (int i = 3; i <= n; i++) {
+		d[i] = max(d[i - 1], max(graph[i - 1] + graph[i] + d[i - 3], d[i - 2] + graph[i]));
+	}
+	cout << d[n] << '\n';
+}
+
+int main() {
+	cin >> n;
+	for (int i = 1; i <= n; i++)cin >> graph[i];
+	dp();
+}
