81048d02b5a38a72a539c3a25d3f102103afdd4f
Kimsuhhee <kimsu3819@gmail.com>
2021-02-13 21:13:43 +0900

    baekjoon:11726_2xn타일링

---
+++ baekjoon_DP/bj11726.cpp
@@ -0,0 +1,25 @@
+﻿/*
+문제:백준11726번
+출처:https://www.acmicpc.net/problem/11726
+*/
+#include<iostream>
+#include<algorithm>
+#include<cstring>
+using namespace std;
+
+int d[1001];
+int n;
+
+void dp() {
+	d[1] = 1; d[2] = 2;
+	for (int i = 3; i <= n; i++) {
+		d[i] = (d[i - 1] + d[i - 2]) % 10007;
+	}
+	cout << d[n] << '\n';
+}
+
+int main() {
+	cin >> n;
+	dp();
+
+}
