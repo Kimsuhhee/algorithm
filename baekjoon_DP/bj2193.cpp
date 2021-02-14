e5274151201d739fc9efe501edeeedd5cc250890
Kimsuhhee <kimsu3819@gmail.com>
2021-02-14 23:58:46 +0900

    baekjoon:2193_이친수

---
+++ baekjoon_DP/bj2193.cpp
@@ -0,0 +1,25 @@
+﻿/*
+문제:백준2193번
+출처:https://www.acmicpc.net/problem/2193
+*/
+#include<iostream>
+#include<algorithm>
+#include<cstring>
+using namespace std;
+
+
+long long d[91];
+int N;
+
+void dp() {
+	d[1] = 1; d[2] = 1; d[3] = 2;
+	for (int i = 4; i <= N; i++) {
+		d[i] = d[i - 1] + d[i - 2];
+	}
+	cout << d[N] << '\n';
+}
+
+int main() {
+	cin >> N;
+	dp();
+}
