a266e94a0154b1affe3023cf9da64270fec4f4a1
Kimsuhhee <kimsu3819@gmail.com>
2021-02-15 23:09:43 +0900

    baekjoon:1904_01타일

---
+++ baekjoon/bj1904.cpp
@@ -0,0 +1,29 @@
+﻿/*
+문제:백준1904번
+출처:https://www.acmicpc.net/problem/1904
+*/
+#include<iostream>
+#include<algorithm>
+#include<cstring>
+using namespace std;
+
+
+long long d[1000001];
+int N;
+
+void dp() {
+	d[1] = 1; d[2] = 2;
+	for (int i = 3; i <= N; i++) {
+		d[i] = (d[i - 1] + d[i - 2]) % 15746;
+	}
+	cout << d[N] << '\n';
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N;
+	dp();
+}
