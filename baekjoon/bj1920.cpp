008b7bf977775a7dd2ca6ca3eda55d9eb312d6a4
Kimsuhhee <kimsu3819@gmail.com>
2021-02-17 23:16:50 +0900

    baekjoon:1920_수찾기

---
+++ baekjoon/bj1920.cpp
@@ -0,0 +1,30 @@
+﻿/*
+문제:백준1920번
+출처:https://www.acmicpc.net/problem/1920
+*/
+#include<iostream>
+#include<algorithm>
+#include<cstring>
+#include<vector>
+using namespace std;
+
+int d[100001];
+int N, M;
+
+int main() {
+	ios_base::sync_with_stdio(0); 
+	cin.tie(0); cout.tie(0);
+	int x;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> d[i];
+	}
+	sort(d, d + N);
+	
+	cin >> M;
+	for (int i = 0; i < M; i++) {
+		cin >> x;
+		if (binary_search(d, d + N, x))cout << 1 << '\n';
+		else cout << 0 << '\n';
+	}
+}
