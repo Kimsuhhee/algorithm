60a2397f00b20ce122eeb0bc2f872e73caa5468c
Kimsuhhee <kimsu3819@gmail.com>
2021-02-17 23:17:14 +0900

    baekjoon:10816_숫자카드2

---
+++ baekjoon/bj10816.cpp
@@ -0,0 +1,32 @@
+﻿/*
+문제:백준10816번
+출처:https://www.acmicpc.net/problem/10816
+*/
+#include<iostream>
+#include<algorithm>
+#include<cstring>
+#include<vector>
+using namespace std;
+
+vector<int>v;
+int card[500001];
+int N, M;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	int x;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> card[i];
+	}
+	sort(card, card + N);
+
+	cin >> M;
+	for (int i = 0; i < M; i++) {
+		cin >> x;
+		auto up = upper_bound(card, card + N, x);
+		auto lo = lower_bound(card, card + N, x);
+		cout << up - lo << " ";
+	}
+}
