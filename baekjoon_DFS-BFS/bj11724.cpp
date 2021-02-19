fedf731891ad7078eb867e9b097dff2e68085d53
Kimsuhhee <kimsu3819@gmail.com>
2021-02-19 22:14:09 +0900

    baekjoon:11724_연결요소의개수

---
+++ baekjoon_DFS-BFS/bj11724.cpp
@@ -0,0 +1,38 @@
+﻿/*
+문제:백준11724번
+출처:https://www.acmicpc.net/problem/11724
+*/
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+vector<int>v[1000];
+bool visited[1000];
+int cnt;
+int N, M;
+
+void dfs(int start) {
+	visited[start] = true;
+	for (int i = 0; i < v[start].size(); i++) {
+		int y = v[start][i];
+		if (!visited[y])dfs(y);
+	}
+}
+
+int main() {
+	int a, b;
+	cin >> N >> M;
+	for (int i = 0; i < M; i++) {
+			cin >> a >> b;
+			v[a].push_back(b);
+			v[b].push_back(a);
+	}
+
+	for (int i = 1; i <= N; i++) {
+		if (!visited[i]) {
+			dfs(i); cnt++;
+		}
+	}
+	cout << cnt << '\n';
+}
+
