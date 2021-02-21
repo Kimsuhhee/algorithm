6aa28af58fa752c82b97a9ec3860fb6a04800f5f
Kimsuhhee <kimsu3819@gmail.com>
2021-02-21 23:39:18 +0900

    baekjoon:1463_1로만들기

---
+++ baekjoon/bj1463.cpp
@@ -0,0 +1,27 @@
+﻿/*
+문제:백준1463번
+출처:https://www.acmicpc.net/problem/1463
+*/
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int d[1000001];
+int N;
+
+void dp() {
+	d[0] = 0; d[1] = 0;
+	for (int i = 2; i <= N; i++) {
+		d[i] = d[i - 1] + 1;
+		if (i % 2 == 0)
+			d[i] = min(d[i], d[i / 2] + 1);
+		if (i % 3 == 0)
+			d[i] = min(d[i], d[i / 3] + 1);
+	}
+	cout << d[N];
+}
+
+int main() {
+	cin >> N;
+	dp();
+}
