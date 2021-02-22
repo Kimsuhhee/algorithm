563cea8931dc7aa60639311a1a1dd08894480538
Kimsuhhee <kimsu3819@gmail.com>
2021-02-22 23:59:51 +0900

    baekjoon:2693_N번째큰수

---
+++ baekjoon_sort/bj2693.cpp
@@ -0,0 +1,24 @@
+﻿/*
+문제:백준2693번
+출처:https://www.acmicpc.net/problem/2693
+*/
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<cstring>
+using namespace std;
+
+int arr[10];
+int T, N;
+
+int main() {
+	cin >> T;
+	while (T--) {
+		for (int i = 0; i < 10; i++) {
+			cin >> arr[i];
+		}
+		sort(arr, arr + 10);
+		cout << arr[7] << '\n';
+		memset(arr, 0, sizeof(arr));
+	}
+}
