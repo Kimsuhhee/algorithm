f9d294cf19f7b349b89c9b61a1aef059f5d28d59
Kimsuhhee <kimsu3819@gmail.com>
2021-02-23 23:41:33 +0900

    baekjoon:1026_보물

---
+++ baekjoon_sort/bj1026.cpp
@@ -0,0 +1,31 @@
+﻿/*
+문제:백준1026번
+출처:https://www.acmicpc.net/problem/1026
+*/
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<cstring>
+using namespace std;
+
+int A[51], B[51];
+int N, answer;
+
+int main() {
+	cin >> N;
+	
+	for (int i = 0; i < N; i++) {
+		cin >> A[i];
+	}
+	for (int i = 0; i < N; i++) {
+		cin >> B[i];
+	}
+
+	sort(A, A + N);
+	sort(B, B + N,greater<int>());
+
+	for (int i = 0; i < N; i++) {
+		answer += A[i] * B[i];
+	}
+	cout << answer << '\n';
+}
