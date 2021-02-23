04c24b27ae9d28abcac6b775c0351eb5abef5f36
Kimsuhhee <kimsu3819@gmail.com>
2021-02-23 23:42:30 +0900

    baekjoon:11656_접미사배열

---
+++ baekjoon_sort/bj11656.cpp
@@ -0,0 +1,28 @@
+﻿/*
+문제:백준11656번
+출처:https://www.acmicpc.net/problem/11656
+*/
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<cstring>
+using namespace std;
+
+vector<string>v;
+string s;
+
+bool compare(string a, string b) {
+	if (a < b)return true;
+	else return false;
+}
+
+int main() {
+	cin >> s;
+	int len = s.length();
+	for (int i = 0; i < len; i++) {
+		string temp=s.substr(i);
+		v.push_back(temp);
+	}
+	sort(v.begin(), v.end(), compare);
+	for (int i = 0; i < v.size(); i++)cout << v[i] << '\n';
+}
