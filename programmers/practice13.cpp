bd479a5eecc6e13ff8827c3b68e01f5cc58af011
Kimsuhhee <kimsu3819@gmail.com>
2021-02-28 00:16:40 +0900

    programmers:연습문제_문자열내마음대로정렬하기

---
+++ programmers/practice13.cpp
@@ -0,0 +1,29 @@
+﻿/*
+문제:프로그래머스 > 코딩테스트연습 > 연습문제 > 문자열 내 마음대로 정렬하기
+출처:https://programmers.co.kr/learn/courses/30/lessons/12915
+*/
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int N;
+
+bool compare(string a, string b) {
+    if (a[N] == b[N]) return a < b;
+    else return a[N] < b[N];
+}
+
+vector<string> solution(vector<string> strings, int n) {
+    N = n;
+    sort(strings.begin(), strings.end(), compare);
+    return strings;
+}
+
+int main() {
+    vector<string> strings = { "sun","bed","car" };
+    strings= solution(strings,1);
+    for (string s : strings)cout << s << " ";
+
+}
