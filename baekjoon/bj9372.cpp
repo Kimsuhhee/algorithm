8d77ab6ad2f8bde141e76c4f7995a33497249983
Kimsuhhee <kimsu3819@gmail.com>
2021-02-27 23:23:45 +0900

    baekjoon:9372_상근이의여행

---
+++ baekjoon/bj9372.cpp
@@ -0,0 +1,22 @@
+﻿/*
+문제:백준9372번
+출처:https://www.acmicpc.net/problem/9372
+*/
+#include<iostream>
+using namespace std;
+
+int T, N, M;
+int a, b;
+
+int main() {
+	cin >> T;
+	while (T--) {
+		cin >> N >> M;
+
+		for (int i = 0; i < M; i++) {
+			cin >> a >> b;
+		}
+		cout << N - 1 << '\n'; //간선의 개수 = 노드수-1
+	}
+
+}
