d1b2fd97df2b7c4e6ab78923d30d34c1afde638d
Kimsuhhee <kimsu3819@gmail.com>
2021-02-28 22:41:40 +0900

    programmers:연습문제_올바른괄호

---
+++ programmers/practice14.cpp
@@ -0,0 +1,34 @@
+﻿/*
+문제:프로그래머스 > 코딩테스트연습 > 연습문제 > 올바른괄호
+출처:https://programmers.co.kr/learn/courses/30/lessons/12909
+*/
+#include <string>
+#include <iostream>
+#include <stack>
+using namespace std;
+
+stack<char>ss;
+
+bool solution(string s)
+{
+    bool answer = true;
+    int cnt = 0;
+
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == '(') {
+            ss.push(s[i]);
+        }
+        else {
+            if (i == 0) {
+                answer = false;
+                break;
+            }
+            else {
+                if (!ss.empty() && ss.top() == '(')ss.pop();
+                else ss.push(s[i]);
+            }
+        }
+    }
+    if (!ss.empty()) return false;
+    else return answer;
+}
