bcf8ba989633ced3c45a125a6dbc3ee316e39643
Kimsuhhee <kimsu3819@gmail.com>
2021-03-02 23:05:05 +0900

    baekjoon:11725_트리의부모찾기

---
+++ baekjoon/bj11725.cpp
@@ -0,0 +1,46 @@
+﻿/*
+문제:백준11725번
+출처:https://www.acmicpc.net/problem/11725
+*/
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<queue>
+using namespace std;
+
+int N, a, b;
+vector<int>v[100001];
+bool visited[100001];
+int parent[100001];
+
+void bfs(int start) {
+	queue<int>q;
+	q.push(start);
+	visited[start] = true;
+	while (!q.empty()) {
+		int x = q.front();
+		q.pop();
+		for (int i = 0; i < v[x].size(); i++) {
+			int y = v[x][i];
+			if (!visited[y]) {
+				q.push(y); 
+				parent[y] = x;
+				visited[y] = true;
+			}
+		}
+	}
+}
+
+int main() {
+	cin >> N;
+	for (int i = 1; i < N ; i++) {
+		cin >> a >> b;
+		v[a].push_back(b);
+		v[b].push_back(a);
+	}
+	bfs(1);
+
+	for (int i = 2; i <= N; i++) {
+		cout << parent[i] << '\n';
+	}
+}
