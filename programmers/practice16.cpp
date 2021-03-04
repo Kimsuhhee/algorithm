d062ef29a672e06c08ae1bf5cae88156427bdeb8
Kimsuhhee <kimsu3819@gmail.com>
2021-03-04 23:50:05 +0900

    programmers:연습문제_2 x n 타일링

---
+++ programmers/practice16.cpp
@@ -0,0 +1,20 @@
+﻿/*
+문제:프로그래머스 > 코딩테스트연습 > 연습문제 > 2 x n 타일링
+출처:https://programmers.co.kr/learn/courses/30/lessons/12900
+*/
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+int dp[60001];
+int solution(int n) {
+    int answer = 0;
+    dp[0] = 0; dp[1] = 1; dp[2] = 2;
+    for (int i = 3; i <= n; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
+    }
+    return dp[n];
+}
+int main() {
+    cout << solution(4) << '\n';
+}
