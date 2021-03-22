96716f3d96cea60e1b35f958b77d308766719dd4
Kimsuhhee <kimsu3819@gmail.com>
2021-03-22 23:47:35 +0900

    baekjoon:11728_배열합치기

---
+++ baekjoon_sort/bj11728.cpp
@@ -0,0 +1,30 @@
+﻿/*
+문제:백준11728번
+출처:https://www.acmicpc.net/problem/11728
+*/
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+int N, M;
+vector<int>v;
+
+int main() {
+	std::ios_base::sync_with_stdio(false);
+	scanf_s("%d %d", &N, &M);
+
+	int num;
+	for (int i = 0; i < N; i++) {
+		scanf_s("%d", &num);
+		v.push_back(num);
+	}
+	for (int i = 0; i < M; i++) {
+		scanf_s("%d", &num);
+		v.push_back(num);
+	}
+	sort(v.begin(), v.end());
+	for (int i = 0; i < v.size(); i++) {
+		printf("%d ", v[i]);
+	}
+}
