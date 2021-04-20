4783604c8de1c599f703718fdff58681f6e543e8
Kimsuhhee <kimsu3819@gmail.com>
2021-04-20 14:34:04 +0900

    baekjoon:2446_별찍기-9

---
+++ 바킹독실전알고리즘/0x02/bj2446.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class bj2446 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i=N;i>0;i--){
+            for(int j=0;j<N-i;j++){
+                System.out.print(" ");
+            }
+            for(int k=0;k<2*i-1;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=1;i<N;i++){
+            for(int j=0;j<N-i-1;j++){
+                System.out.print(" ");
+            }
+            for(int k=0;k<2*i+1;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
