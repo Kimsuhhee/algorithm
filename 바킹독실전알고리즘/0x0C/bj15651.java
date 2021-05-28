ad88e64bf1fe5581423800ebb905edd1b8704c40
Kimsuhhee <kimsu3819@gmail.com>
2021-05-28 23:24:01 +0900

    baekjoon:15651_N과 M (3)

---
+++ 바킹독실전알고리즘/0x0C/bj15651.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bj15651 {
+    static int N,M;
+    static int[]arr = new int[9];
+    static boolean[]visited = new boolean[9];
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(br.readLine()," ");
+        N = Integer.parseInt(stk.nextToken());
+        M = Integer.parseInt(stk.nextToken());
+
+        func(0);
+        System.out.println(sb);
+
+    }
+
+    private static void func(int n) {
+        if(n==M){
+            for(int i=0;i<M;i++){
+                sb.append(arr[i]+" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for(int i=1;i<=N;i++){
+            if(!visited[i]){
+                arr[n] = i;
+                func(n+1);
+            }
+        }
+    }
+}
