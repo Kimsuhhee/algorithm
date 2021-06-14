e2ed2095c6ac7fcc1a570cc2e4b9cd5c8d59d631
Kimsuhhee <kimsu3819@gmail.com>
2021-06-14 23:45:46 +0900

    baekjoon:15666_N과 M (12)

---
+++ 바킹독실전알고리즘/0x0C/bj15666.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class bj15666 {
+    static int N,M;
+    static LinkedHashSet<String>set = new LinkedHashSet<>();
+    static List<Integer> list = new ArrayList<>();
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(br.readLine()," ");
+        N = Integer.parseInt(stk.nextToken());
+        M = Integer.parseInt(stk.nextToken());
+
+        arr = new int[M];
+
+        stk = new StringTokenizer(br.readLine()," ");
+        while(stk.hasMoreTokens())
+            list.add(Integer.parseInt(stk.nextToken()));
+        Collections.sort(list);
+
+        func(0,0);
+        for(String str:set) System.out.println(str);
+    }
+
+    private static void func(int start, int n) {
+        if(n==M){
+            String s = "";
+            for(int i=0;i<arr.length;i++)
+                s+=arr[i]+" ";
+            set.add(s);
+            return;
+        }
+        for(int i=start;i<N;i++){
+                arr[n] = list.get(i);
+                func(i,n+1);
+            }
+        }
+    }
